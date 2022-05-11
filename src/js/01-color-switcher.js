7c638c1cd6a74f3f9b6ef9a91492b91c21d12a9d
Ann Sarviro <Annka_nuha@i.ua>
2022-05-12 01:55:08 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartSwitcher);
+stopBtn.addEventListener('click', onStopSwitcher);
+let timerId = null;
+
+function onStartSwitcher() {
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    console.log('вызвали функцию')
+    startBtn.disabled = true;
+};
+
+
+function onStopSwitcher() {
+    clearInterval(timerId);
+    timerId = null;
+    startBtn.disabled = false;
+};
+
+
+
+
+
+console.log(startBtn)
+
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
